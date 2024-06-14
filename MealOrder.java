db30e1f43792fa9f7079814e73cce74e10ec8603
karnwine729 <31371047+karnwine729@users.noreply.github.com>
2024-06-14 15:22:04 -0400

    Create MealOrder.java

---
+++ MealOrder.java
@@ -0,0 +1,11 @@
+public class MealOrder {
+    private Burger burger;
+    private Drink drink;
+    private Side side;
+    
+    public MealOrder() {
+        this.burger = 
+        this.drink = 
+        this.side = 
+    }
+}
